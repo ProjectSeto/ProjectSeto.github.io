c30a554376a542acff0b675867fafdbd6b502be2
ProjectSeto <155964212+ProjectSeto@users.noreply.github.com>
2024-01-08 17:45:19 +0700

    Add files via upload

---
+++ English/webinggris.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Menu Utama</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
+</head>
+<style>
+* {
+	padding: 0;
+	margin: 0;
+	font-family: 'Quicksand', sans-serif;
+}
+body {
+	background-image: url(coba.jpg);
+	background-repeat: no-repeat;
+	background-size: cover;
+	background-color: #f8f8f8;
+	background-attachment: fixed;
+}
+a {
+	color: inherit;
+	text-decoration: none;
+}
+.input-control {
+	width: 100%;
+	padding: 10px;
+	margin-bottom: 15px;
+	box-sizing: border-box;
+}
+header {
+	background-color: black;
+	color: #fff;
+}
+header h1 {
+	float: left;
+	padding: 10px 0;
+}
+header ul {
+	float: right;
+}
+header ul li {
+	display: inline-block;
+} 
+header ul li a {
+	padding: 35px 25px;
+	display: inline-block;
+}
+footer {
+	padding: 25px 0;
+}
+.container {
+	width: 80%;
+	margin: 0 auto;
+}
+.container1 {
+	width: 100%;
+	margin: 0 auto;
+}
+.container:after {
+	content: '';
+	display: block;
+	clear: both;
+}
+.section {
+	padding: 25px 0;
+}
+.box {
+	background-color: whitesmoke;
+	border: 1px solid #ccc;
+	padding: 35px;
+	box-sizing: border-box;
+	margin: 10px 0 25px 0;
+}
+.btn {
+	width: 350px;
+    height: 50px;
+    background-color: whitesmoke;
+    color: black;
+    font-size: 17px;
+}
+footer {
+	background-color: black;
+	color: #fff;
+}
+footer h1 {
+	float: left;
+	padding: 80px 0;
+}
+footer ul {
+	float: right;
+}
+footer ul li {
+	display: inline-block;
+} 
+footer ul li a {
+	padding: 35px 25px;
+	display: inline-block;
+}
+</style>
+<body>
+	<header>
+		<div class="container">
+		<h1><a href="webinggris.php" style="color:white;">GROUP 10</a></h1>
+		<ul>
+			<li><a href="project1.php" style="color:white;">Project 1</a></li>
+			<li><a href="project2.php" style="color:white;">Project 2</a></li>
+			<li><a href="project3.php" style="color:white;">Project 3</a></li>
+		</ul>
+		</div>
+	</header>
+
+	<div class="section">
+		<div class="container">
+			<div class="box" >
+				<h3 style=" color:black;">Welcome to the Group 10 Website</h3>
+			</div>
+		</div>
+	</div>
+		<div class="section">
+		<div class="container">
+			<div class="box" >
+				<h3 style=" color:black;">Member of Group 10 :</h3><br>
+				<h3 style=" color:black;">1. Irham maulana</h3>
+				<h3 style=" color:black;">2. jaenal</h3>
+				<h3 style=" color:black;">3. Edi Dwi Seto</h3>
+				<h3 style=" color:black;">4. Habil Fahrizal</h3>
+				<h3 style=" color:black;">5. Syahlah Fadhlik Darmawan</h3>
+			</div>
+		</div>
+	</div>
+	<footer>
+	<div class="container">
+		<h1><a href="index.php" style="color:white;"></a></h1>
+		
+	</div>	
+	</footer>	
+</body>
+</html>
